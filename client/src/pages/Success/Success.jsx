4b354e0fbfb504f3966cfb3fc94e641ca9749052
johnhaab <wearedevelopersbiz@gmail.com>
2023-12-18 22:27:26 -0500

    refactor: remove swal alerts and changed data fetching method

--- client/src/pages/Success/Success.jsx
+++ client/src/pages/Success/Success.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
 
 const Success = () => {
   const [data, setData] = useState({
@@ -15,49 +14,42 @@ const Success = () => {
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (data.id === null) {
-      // Show the alert if there's no data
-
-      Swal.fire({
-        title: "We found no data?",
-        text: "You might want to try and verify a code to see some data here!",
-        icon: "question",
-        confirmButtonText: `
-        <a href="/" >Back to homepage</a>
-        `,
-      });
-    }
-  });
-
   return (
     <div className="px-20 flex justify-center items-center h-screen flex-col gap-10">
+      <button
+        className="fix-btn absolute top-8 bg-[#01b170] hover:scale-105 transition-all text-white rounded-md px-4 py-3 font-semibold"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        return to homepage{" "}
+      </button>
       {data.id !== null ? (
         <>
           <h1 className="font-bold">Output:</h1>
-          <div className=" bg-black bg-opacity-20 p-5 rounded-md font-semibold">
+          <div className="border-gray-400 border-2 p-5 rounded-md font-semibold">
             <p>
-              ID: <span className="text-lime-600 font-bold">{data.id}</span>
+              ID: <span className="text-[#01b170] font-bold">{data.id}</span>
             </p>
             <p>
               Date the code was created:{" "}
-              <span className="text-lime-600 font-bold">{data.createdAt}</span>
+              <span className="text-[#01b170] font-bold">{data.createdAt}</span>
             </p>
             <p>
               Email linked to the code:{" "}
-              <span className="text-lime-600 font-bold">{data.email}</span>
+              <span className="text-[#01b170] font-bold">{data.email}</span>
             </p>
             <p>
               How many seconds until the code expires:{" "}
-              <span className="text-lime-600 font-bold">{data.expTime}</span>
+              <span className="text-[#01b170] font-bold">{data.expTime}</span>
             </p>
             <p>
               The code itself:{" "}
-              <span className="text-lime-600 font-bold">{data.code}</span>
+              <span className="text-[#01b170] font-bold">{data.code}</span>
             </p>
             <p>
               The IP Address that activated the code:{" "}
-              <span className="text-lime-600 font-bold">
+              <span className="text-[#01b170] font-bold">
                 {data.activatedIp}
               </span>
             </p>
@@ -68,15 +60,6 @@ const Success = () => {
           <p>No data found please verify a code.</p>
         </>
       )}
-
-      <button
-        className="absolute top-0 mt-4 underline text-lime-600 font-semibold"
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        return to homepage{" "}
-      </button>
     </div>
   );
 };
