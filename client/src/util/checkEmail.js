90aa6e891a6ece76a6604a5546fbf894ced48c8d
johnhaab <wearedevelopersbiz@gmail.com>
2023-12-18 22:23:49 -0500

    docs: added better comments

--- client/src/util/checkEmail.js
+++ client/src/util/checkEmail.js
@@ -1,4 +1,9 @@
-// Simple function to check if a string is a valid email address
+/**
+ * Simple function to check if a string is a valid email address.
+ *
+ * @param {string} inputText - The email that will be checked.
+ */
+
 export default function checkEmail(inputText) {
   // Define a regular expression pattern for email validation
   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
